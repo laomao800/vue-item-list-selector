081e2fd13ecde87e3899cff63bbbbe856ae5467c
xiezhihong <xiezhihong@banggood.com>
2018-02-13 11:00:30 +0800

    demo update

--- app/App.vue
+++ app/App.vue
@@ -1,16 +1,11 @@
 <template>
-  <div id="app">
+  <div id='app'>
     {{ selected }}
-    <item-list-selector :data="listData" v-model="selected" :use-page="true">
-      <div slot="option" slot-scope="props">
-        <div><strong>label:</strong><span>{{ props.item.label }}</span></div>
-        <div><strong>value:</strong><span>{{ props.item.value }}</span></div>
-      </div>
-    </item-list-selector>
+    <item-list-selector :data='listData' v-model='selected' :use-page='true'></item-list-selector>
   </div>
 </template>
 
-<script lang="ts">
+<script lang='ts'>
   import Vue from 'vue'
   import Component from 'vue-class-component'
 
@@ -20,29 +15,20 @@
     components: { itemListSelector }
   })
   export default class App extends Vue {
-    listData = [
-    { label: '小刘00', value: '00' },
-    { label: '小张01', value: '01' },
-    { label: '小刘02', value: '02' },
-    { label: '小张03', value: '03' },
-    { label: '小刘04', value: '04' },
-    { label: '小张05', value: '05' },
-    { label: '小刘06', value: '06' },
-    { label: '小张07', value: '07' },
-    { label: '小刘08', value: '08' },
-    { label: '小张09', value: '09' },
-    { label: '小刘10', value: '10' },
-    { label: '小张11', value: '11' },
-    { label: '小刘12', value: '12' },
-    { label: '小张13', value: '13' },
-    { label: '小刘14', value: '14' },
-    { label: '小刘15', value: '15' },
-    { label: '小刘16', value: '16' },
-    { label: '小刘17', value: '17' },
-    { label: '小刘18', value: '18' },
-    { label: '小张19', value: '19' },
-    { label: '小张20', value: '20' }
-    ]
     selected = []
+    listData = [
+      { label: 'January', value: 0 },
+      { label: 'February', value: 1 },
+      { label: 'March', value: 2 },
+      { label: 'April', value: 3 },
+      { label: 'May', value: 4 },
+      { label: 'June', value: 5 },
+      { label: 'July', value: 6 },
+      { label: 'August', value: 7 },
+      { label: 'September', value: 8 },
+      { label: 'October', value: 9 },
+      { label: 'November', value: 10 },
+      { label: 'December', value: 11 }
+    ]
   }
 </script>
